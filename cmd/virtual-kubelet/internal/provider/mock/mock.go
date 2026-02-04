994184ce25e01a99985c88c2148eb5129b264f1b
dciangot <dciangot@cern.ch>
2026-02-04 12:29:05 +0100

    e2e: guard notifier and prefer KUBECONFIG; avoid podcontroller panic
    Signed-off-by: dciangot <dciangot@cern.ch>

--- cmd/virtual-kubelet/internal/provider/mock/mock.go
+++ cmd/virtual-kubelet/internal/provider/mock/mock.go
@@ -83,6 +83,8 @@ func NewMockProviderMockConfig(config MockConfig, nodeName, operatingSystem stri
 		pods:               make(map[string]*v1.Pod),
 		config:             config,
 		startTime:          time.Now(),
+		// default notifier is a no-op to avoid nil deref if NotifyPods isn't called yet
+		notifier: func(*v1.Pod) {},
 	}
 
 	return &provider, nil
